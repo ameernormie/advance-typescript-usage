f3fd6636981b9ba60a5dd415dd998a90e1154b79
Ameer Hamza <ameernormie@gmail.com>
2020-04-25 11:21:22 +0500

    feat: learn function overloading in typescript

--- app.ts
+++ app.ts
@@ -19,3 +19,24 @@ function sayIt(person: Person) {
 }
 
 console.log(sayIt(mike));
+
+console.log(
+  "********************Function Overloading (Type Guards)**********************"
+);
+
+function getTitles(author: string): string[];
+function getTitles(available: boolean): string[];
+function getTitles(bookProperty: any): string[] {
+  let returnValue: Array<string> = [];
+  if (typeof bookProperty === "boolean") {
+    // Return based on available
+    returnValue = ["available number one"];
+  }
+  if (typeof bookProperty === "string") {
+    returnValue = ["Author based"];
+  }
+  return returnValue;
+}
+
+console.log(getTitles("StringParam"));
+console.log(getTitles(true));
